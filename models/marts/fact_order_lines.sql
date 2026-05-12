4dd6d964a8c0ba791ee58afb0386ff34fcbee6f2
Hristina Todorova <hristina.todorova@gmail.com>
2026-05-12 21:29:49 +0200

    Update dbt workflow, fact_order_lines, sources, and packages

--- models/marts/fact_order_lines.sql
+++ models/marts/fact_order_lines.sql
@@ -5,6 +5,12 @@
 --          each line with product category from dim_products.
 
 select
+    {{ dbt_utils.generate_surrogate_key([
+        'od.order_id',
+        'od.line_id',
+        'od.product_id'
+    ]) }} as order_line_key,
+
     od.order_id,
     od.line_id,
     od.product_id,
@@ -14,11 +20,13 @@ select
     od.discount_pct,
 
     od.quantity * od.unit_price as line_gross,
-    od.quantity * od.unit_price * (1 - od.discount_pct / 100) as line_net,
+    od.quantity * od.unit_price * (
+        1 - od.discount_pct / 100
+    ) as line_net,
+
     p.product_id is not null as has_valid_product,
     p.category as product_category
 
 from {{ ref('stg_order_details') }} od
-
 left join {{ ref('dim_products') }} p
     on od.product_id = p.product_id
